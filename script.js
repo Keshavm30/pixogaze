df0d6276bd42fac46a3417afed9320141b8a7ca8
Keshavm30 <keshavm2007@gmail.com>
2024-09-16 01:22:15 +0530

    .

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("Message sent!");
+    });
+
+    // Smooth scrolling
+    const navLinks = document.querySelectorAll("nav a");
+    navLinks.forEach(link => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            document.querySelector(event.target.getAttribute("href")).scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+});
+
+
+function mobile_navigation_toggle() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
